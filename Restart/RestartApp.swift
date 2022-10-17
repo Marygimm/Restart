0256c44a5982466c42d10d2e780cef57d75c23ef
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-17 09:56:52 +0000

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Mary Moreira on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
